babb9e8332b519aa7f9d126e0760e8bf4b9d86d4
riccardo <riccardomanzi1994@gmail.com>
2025-07-23 11:40:58 +0200

    Initial commit

---
+++ poc-poleepo-web/src/main/java/com/poleepo/PocPoleepoApplication.java
@@ -0,0 +1,13 @@
+package com.poleepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPoleepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocPoleepoApplication.class, args);
+	}
+
+}
