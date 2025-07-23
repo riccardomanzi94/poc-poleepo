31514baff5b58ec222b6fb5f69d4508feaf10f2b
Manzi Riccardo <riccardo.manzi@fabrick.com>
2025-07-23 15:46:27 +0200

    Add caching support and update application properties

--- poc-poleepo-web/src/main/java/com/poleepo/PocPoleepoApplication.java
+++ poc-poleepo-web/src/main/java/com/poleepo/PocPoleepoApplication.java
@@ -1,10 +1,11 @@
 package com.poleepo;
 
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PocPoleepoApplication {
 
 	public static void main(String[] args) {
