33338556f056fceb65ea6a4de121ac7353dc5e12
riccardo <riccardomanzi1994@gmail.com>
2025-07-26 13:32:27 +0200

    feat: add interceptor for logging uuid request

---
+++ poc-poleepo-web/src/main/java/com/poleepo/controller/LoggingInterceptor.java
@@ -0,0 +1,21 @@
+package com.poleepo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info("Request URI: {} ", request.getRequestURI());
+        log.info("X-Correlation-ID: {} ", UUID.randomUUID());
+        return true;
+    }
+}
